e06942d4755671c03531a72109fe126922f4a15b
HinataDeveloper <StubbornStruggle@gmail.com>
2026-09-03 11:59:24 +0330

    Using iterator: Using map() method

---
+++ src/snapshots/main_025_20260903_115837.rs
@@ -0,0 +1,43 @@
+// Date: Thu Sep 03 2026
+
+// Project: Learning Chapter 13
+// Goal: Using Interator
+// Dependency: Without dependency
+
+// rustc 1.100.0-nightly (5db7f4be8 2026-09-01)
+// binary: rustc
+// commit-hash: 5db7f4be8a36c1b8ae19299469e2be2b0f052c21
+// commit-date: 2026-09-01
+// host: x86_64-unknown-linux-gnu
+// release: 1.100.0-nightly
+// LLVM version: 23.1.0
+
+// cargo 1.100.0-nightly (e8cb624d5 2026-08-22)
+// release: 1.100.0-nightly
+// commit-hash: e8cb624d5701824f46a2ec5873cfd59ee3d2f66c
+// commit-date: 2026-08-22
+// host: x86_64-unknown-linux-gnu
+// libgit2: 1.9.6 (sys:0.21.0 vendored)
+// libcurl: 8.21.0-DEV (sys:0.4.90+curl-8.21.0 vendored ssl:OpenSSL/3.6.3)
+// ssl: OpenSSL 3.6.3 9 Jun 2026
+// os: Fedora 44.0.0 [64-bit]
+
+// Kernel Version: 7.1.12-200.fc44.x86_64
+// Firmware Version: 71CN51WW(V1.21)
+
+fn main() {
+    println!("\n");
+
+    let number_list: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    let v1: Vec<i32> = number_list.iter().map(|d| d + 10).collect();
+
+    assert_eq!(v1, vec![11, 12, 13, 14, 15, 16, 17, 18, 19]);
+    println!("assertion check was passed ...");
+
+    number_list.iter().for_each(|d| {
+        println!("{}", d);
+    });
+
+    println!("\nThe End ...\n");
+}
